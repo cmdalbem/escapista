454d6c83027e51053a90cbbbc7e8284bde7cbf70
cmdalbem <cristiano.dalbem@gmail.com>
2020-09-05 16:18:42 -0300

    Update Fullscreen icon

--- src/IconFullScreen.js
+++ src/IconFullScreen.js
@@ -4,38 +4,25 @@ function IconFullScreen(props) {
     const { isFullScreen } = props;
 
     return (
-        isFullScreen ?
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="none"
-                viewBox="0 0 16 16"
-            >
-                <path
-                    stroke="#333"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.33"
-                    d="M2.667 9.334h4v4M13.333 6.667h-4v-4M9.333 6.667L14 2M2 14l4.667-4.666"
-                ></path>
-            </svg>
-            :
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="none"
-                viewBox="0 0 16 16"
-            >
-                <path
-                    stroke="#333"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.33"
-                    d="M10 2h4v4M6 14H2v-4M14 2L9.333 6.667M2 14l4.667-4.666"
-                ></path>
-            </svg>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="none"
+            viewBox="0 0 16 16"
+        >
+            <path
+                stroke="#333"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.333"
+                d={isFullScreen ?
+                    "M5.333 2v2c0 .736-.597 1.333-1.333 1.333H2M14 5.333h-2A1.333 1.333 0 0110.667 4V2M10.667 14v-2c0-.736.597-1.334 1.333-1.334h2M2 10.666h2c.736 0 1.333.598 1.333 1.334v2"
+                    :
+                    "M5.333 2h-2C2.597 2 2 2.597 2 3.333v2M14 5.333v-2C14 2.597 13.403 2 12.667 2h-2M10.667 14h2c.736 0 1.333-.597 1.333-1.334v-2M2 10.666v2C2 13.403 2.597 14 3.333 14h2"
+                }
+            ></path>
+        </svg>
     );
 }
 
