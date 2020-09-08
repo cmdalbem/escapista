5a00d863686669781aa1e79c416bfbce47fe2b0e
cmdalbem <cristiano.dalbem@gmail.com>
2020-09-08 09:54:51 -0300

    Fix non-JSX attributes on spinner SVG

--- src/Spinner.js
+++ src/Spinner.js
@@ -5,17 +5,17 @@ import './Spinner.css'
 // Thanks: https://codepen.io/jczimm/pen/vEBpoL
 export default function () {
     return (
-        <svg class="circular" viewBox="25 25 50 50">
+        <svg className="circular" viewBox="25 25 50 50">
             <circle
-                class="path"
                 cx="50"
                 cy="50"
                 r="20"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-miterlimit="10"
-            />
+                strokeMiterlimit="10"
+                strokeWidth="2"
+                className="path"
+            ></circle>
         </svg>
     )
 }
