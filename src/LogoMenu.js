7ea9a9643f2a86b1a766bbcfce8b91df519133ca
cmdalbem <cristiano.dalbem@gmail.com>
2020-09-18 11:46:49 -0300

    Update typography to Mulish

--- src/LogoMenu.js
+++ src/LogoMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Slant as Hamburger } from 'hamburger-react'
+import { Fade as Hamburger } from 'hamburger-react'
 import { isMobile } from "react-device-detect";
 
 import { ESCAPIST_EASING_BEZIER } from './constants.js'
@@ -11,12 +11,15 @@ export default function LogoMenu(props) {
     onToggleUI
   } = props;
 
+  // const logoTypography = 'text-2xl unna';
+  const logoTypography = 'text-lg noto tracking-widest uppercase font-bold';
+
   return (
     <button
       className={`
-        fixed top-0 left-0 z-1 flex items-center pt-1
+        fixed top-0 left-0 z-1 flex items-center pt-1 
         cursor-pointer hover:opacity-75 focus:opacity-75
-        ${isMobile ? 'mt-2 ml-2' : 'mt-8 ml-8'}
+        ${isMobile ? 'mt-2 ml-2' : 'mt-8 ml-16'}
         ${isUIVisible ? 'text-green-900' : 'text-white'}`
       }
       onClick={onToggleUI}
@@ -30,14 +33,14 @@ export default function LogoMenu(props) {
 
       <h1
         className={`
-            inline-block text-2xl leading-6 unna
+            inline-block leading-6 ${logoTypography} text-white
             ${isMobile ? 'ml-2' : 'ml-4'}
           `}
-        style={{
-          transition: isUIVisible
-            ? `color .5s ${ESCAPIST_EASING_BEZIER} 1.3s`
-            : `color .5s ${ESCAPIST_EASING_BEZIER} 1.3s`
-        }}
+        // style={{
+        //   transition: isUIVisible
+        //     ? `color .5s ${ESCAPIST_EASING_BEZIER} 1.3s`
+        //     : `color .5s ${ESCAPIST_EASING_BEZIER} 1.3s`
+        // }}
       >
         Escapista
       </h1>
