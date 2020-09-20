23ab7a67825526dca3a22927158ce4a80216419a
cmdalbem <cristiano.dalbem@gmail.com>
2020-09-20 19:45:36 -0300

    Art component refactor

--- src/Art.js
+++ src/Art.js
@@ -4,14 +4,6 @@ import ArtSVG from './assets/ArtSVG.js'
 
 import { throttle } from './utils.js';
 
-import {
-    MAIN_BAR_WIDTH,
-    BOTTOM_BAR_HEIGHT,
-    ESCAPIST_EASING_BEZIER,
-    ESCAPIST_EASING_TIMING
-} from './constants.js';
-
-
 class Art extends React.Component {
     constructor(props) {
         super(props);
@@ -23,10 +15,13 @@ class Art extends React.Component {
         const y = e.clientY;
         const layers = document.querySelectorAll('.parallax-layer');
 
+        const xMult = 1;
+        const yMult = 3;
+
         for (var j = 0; j < layers.length; j++) {
             const deep = layers[j].getAttribute('parallaxDeep');
-            const itemX = x / deep;
-            const itemY = y / deep;
+            const itemX = x * xMult / deep;
+            const itemY = y * yMult / deep;
             
             layers[j].style.transform = 'translateX(' + itemX + '%) translateY(' + itemY + '%)';
         }
@@ -45,26 +40,8 @@ class Art extends React.Component {
     }
 
     render() {
-        // const { isUIVisible } = this.props;
-        const isUIVisible = true;
-
         return (
-            <div className="overflow-hidden">
-                <div
-                    className="bg-gray-100 flex h-screen items-end justify-start w-screen"
-                    style={{
-                        transition: `transform ${ESCAPIST_EASING_TIMING} ${ESCAPIST_EASING_BEZIER}`,
-                        transform: isUIVisible ? 
-                            `translate(${MAIN_BAR_WIDTH}px, -${BOTTOM_BAR_HEIGHT}px)`:
-                            `translate(0, 0)`,
-                        paddingRight: isUIVisible && MAIN_BAR_WIDTH,
-                        paddingTop: isUIVisible && BOTTOM_BAR_HEIGHT
-                    }}>
-                    <div className="w-full">
-                        <ArtSVG/>
-                    </div>
-                </div>
-            </div>
+            <ArtSVG/>
         );
     }
 }
