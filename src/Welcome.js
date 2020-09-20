9370ac08c6e98e33b81d72a4449e10395cddc756
cmdalbem <cristiano.dalbem@gmail.com>
2020-09-20 19:44:48 -0300

    Welcome screen transitions

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from "react";
 
+import { withTranslation } from 'react-i18next';
+
 import Art from './Art.js'
 
 import {
     BOTTOM_BAR_HEIGHT,
     MAIN_BAR_WIDTH,
-    ESCAPIST_EASING_BEZIER
+    ESCAPIST_EASING_BEZIER,
+    ESCAPIST_EASING_TIMING,
 } from './constants.js';
 
+
 function Welcome(props) {
+    const { t } = props;
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        setTimeout(() => setLoading(false), 1000);
+        // setTimeout(() => setLoading(false), 1000);
+        setLoading(false);
     }, []);
 
     function onStartClick() {
@@ -32,45 +38,64 @@ function Welcome(props) {
                 className="fixed z-10 pt-16 pl-16 noto w-screen h-full text-green-900 flex flex-col items-start justify-between"
                 style={{paddingBottom: BOTTOM_BAR_HEIGHT}}
             >
-                <div className={`${slideIn} delay-300`}>
+                <div className={`${slideIn}`}>
                     <h1 className={logoTypography}>
                         Escapista
                     </h1>
 
-                    <h2 className="mt-16 font-extrabold text-6xl leading-tight">
-                        Para onde você<br/>quer escapar?
+                    <h2 className="mt-16 max-w-xl font-extrabold text-6xl leading-tight">
+                        { t('welcome-heading') }
                     </h2>
 
-                    <div className="mt-12 pr-24" style={{width: MAIN_BAR_WIDTH}}>
-                        <p className="mt-2">
-                            Este é sua oportunidade de escapar para novos e diferentes lugares. Escolha uma categoria e deixe sua mente vagar por aí com a programação que curamos carinhosamente para você.
+                    <div className="mt-12 pr-24 text-sm" style={{width: MAIN_BAR_WIDTH}}>
+                        <p className="mt-3">
+                            { t('welcome-body1') }
                         </p>
 
-                        <p className="mt-2">
-                            Deixe rolando enquanto trabalha, ponha na TV da sala ou combine com amigos para passearem juntos durante uma call.
+                        <p className="mt-3">
+                            { t('welcome-body2') }
+                        </p>
+
+                        <p className="mt-3">
+                            { t('welcome-body3') }
                         </p>
                         
                     </div>
                 </div>
 
-                <div className={`${slideIn} delay-500`}>
+                <div className={`${slideIn} delay-200`}>
                     <button
                         className="py-3 px-6 bg-green-700 rounded text-white hover:bg-green-800 duration-300"
                         onClick={onStartClick}>
-                        Começar
+                        { t('welcome-cta') }
                     </button>
 
-                    <button className="py-3 px-6 ml-2 rounded hover:bg-gray-200 duration-300">
-                        Manifesto
-                    </button>
+                    <a
+                        href="https://www.notion.so/Manifesto-dac06fb54a264b719a299f06705f15bf"
+                        className="py-3 px-6 ml-2 rounded hover:bg-gray-200 duration-300"
+                        target="_blank" rel="noopener noreferrer"
+                    >
+                        { t('manifesto') }
+                    </a>
                 </div>
             </div>
 
-            <div className={`flex transition-opacity duration-1000 delay-300 ${loading ? 'opacity-0' : 'opacity-100'}`}>
-                <Art />
+            <div className={`overflow-hidden`}>
+                <div
+                    className="bg-gray-100 flex h-screen items-end justify-start w-screen"
+                    style={{
+                        transition: `transform ${ESCAPIST_EASING_TIMING} ${ESCAPIST_EASING_BEZIER}`,
+                        transform: `translate(${MAIN_BAR_WIDTH}px, -${BOTTOM_BAR_HEIGHT}px)`,
+                        paddingRight: MAIN_BAR_WIDTH,
+                        paddingTop: BOTTOM_BAR_HEIGHT
+                    }}>
+                    <div className={`w-full transition-opacity duration-1000 delay-500 ${loading ? 'opacity-0' : 'opacity-100'}`}>
+                        <Art />
+                    </div>
+                </div>
             </div>
         </div>
     );
 }
 
-export default Welcome;
+export default withTranslation()(Welcome);
