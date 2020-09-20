ac169b86b3729bac60b83170624aad502b60a1a8
cmdalbem <cristiano.dalbem@gmail.com>
2020-09-20 13:08:06 -0300

    Improvements on parallax and transition in

--- src/Art.js
+++ src/Art.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-import ArtSVG from './assets/Art.js'
+import ArtSVG from './assets/ArtSVG.js'
 
 import { throttle } from './utils.js';
 
 import {
     MAIN_BAR_WIDTH,
     BOTTOM_BAR_HEIGHT,
-    LIVENESS_CHECK_MS,
-    VIDEO_TRANSITION_MS,
     ESCAPIST_EASING_BEZIER,
     ESCAPIST_EASING_TIMING
 } from './constants.js';
@@ -19,30 +17,17 @@ class Art extends React.Component {
         super(props);
     }
 
+    // Inspired by Eugene Burlak
     onMouseMove(e) {
-        const selectors = {
-            layers: '.parallax-layer',
-            deep: 'parallaxDeep'
-        };
-        
         const x = e.clientX;
         const y = e.clientY;
-        const layers = document.querySelectorAll(selectors.layers);
-
-        // console.debug(x,y);
-        console.debug(layers);
+        const layers = document.querySelectorAll('.parallax-layer');
 
         for (var j = 0; j < layers.length; j++) {
-            const deep = layers[j].getAttribute(selectors.deep);
-            const disallow = layers[j].getAttribute('data-parallax-disallow');
-            const itemX = (disallow && disallow === 'x') ? 0 : x / deep;
-            const itemY = (disallow && disallow === 'y') ? 0 : y / deep;
+            const deep = layers[j].getAttribute('parallaxDeep');
+            const itemX = x / deep;
+            const itemY = y / deep;
             
-            console.debug(deep);
-            console.debug(itemX,itemY);
-
-            if (disallow && disallow === 'both') return;
-
             layers[j].style.transform = 'translateX(' + itemX + '%) translateY(' + itemY + '%)';
         }
     }
@@ -66,7 +51,7 @@ class Art extends React.Component {
         return (
             <div className="overflow-hidden">
                 <div
-                    className="absolute w-full h-full bg-gray-100 flex items-end justify-start"
+                    className="bg-gray-100 flex h-screen items-end justify-start w-screen"
                     style={{
                         transition: `transform ${ESCAPIST_EASING_TIMING} ${ESCAPIST_EASING_BEZIER}`,
                         transform: isUIVisible ? 
@@ -75,10 +60,9 @@ class Art extends React.Component {
                         paddingRight: isUIVisible && MAIN_BAR_WIDTH,
                         paddingTop: isUIVisible && BOTTOM_BAR_HEIGHT
                     }}>
-                    
-                    {/* <div className="h-10 w-10 text-gray-400"> */}
+                    <div className="w-full">
                         <ArtSVG/>
-                    {/* </div> */}
+                    </div>
                 </div>
             </div>
         );
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Art from './Art.js'
 
-import { BOTTOM_BAR_HEIGHT, MAIN_BAR_WIDTH } from './constants.js';
+import {
+    BOTTOM_BAR_HEIGHT,
+    MAIN_BAR_WIDTH,
+    ESCAPIST_EASING_BEZIER
+} from './constants.js';
 
 function Welcome(props) {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000);
+    }, []);
+
+    function onStartClick() {
+        setLoading(true);
+        setTimeout(props.onStartClick, 2000);
+    }
+
     const logoTypography = 'text-lg noto tracking-widest uppercase font-bold';
+
+    const slideIn = `
+        transition-all transform duration-1000 ease-in-out
+        ${loading ? '-translate-y-3 opacity-0' : '-translate-y-0 opacity-100'}`;
 
     return (
         <div>
@@ -13,7 +32,7 @@ function Welcome(props) {
                 className="fixed z-10 pt-16 pl-16 noto w-screen h-full text-green-900 flex flex-col items-start justify-between"
                 style={{paddingBottom: BOTTOM_BAR_HEIGHT}}
             >
-                <div>
+                <div className={`${slideIn} delay-300`}>
                     <h1 className={logoTypography}>
                         Escapista
                     </h1>
@@ -22,7 +41,7 @@ function Welcome(props) {
                         Para onde você<br/>quer escapar?
                     </h2>
 
-                    <div className="mt-8 pr-24" style={{width: MAIN_BAR_WIDTH}}>
+                    <div className="mt-12 pr-24" style={{width: MAIN_BAR_WIDTH}}>
                         <p className="mt-2">
                             Este é sua oportunidade de escapar para novos e diferentes lugares. Escolha uma categoria e deixe sua mente vagar por aí com a programação que curamos carinhosamente para você.
                         </p>
@@ -34,12 +53,22 @@ function Welcome(props) {
                     </div>
                 </div>
 
-                <button className="py-3 px-6 bg-green-700 rounded text-white" onClick={props.onStartClick}>
-                    Começar
-                </button>
+                <div className={`${slideIn} delay-500`}>
+                    <button
+                        className="py-3 px-6 bg-green-700 rounded text-white hover:bg-green-800 duration-300"
+                        onClick={onStartClick}>
+                        Começar
+                    </button>
+
+                    <button className="py-3 px-6 ml-2 rounded hover:bg-gray-200 duration-300">
+                        Manifesto
+                    </button>
+                </div>
             </div>
 
-            <Art />
+            <div className={`flex transition-opacity duration-1000 delay-300 ${loading ? 'opacity-0' : 'opacity-100'}`}>
+                <Art />
+            </div>
         </div>
     );
 }
