ecec4d68e07b9bfe78a3aa39fbb2f57cc1f416ca
cmdalbem <cristiano.dalbem@gmail.com>
2020-12-23 17:21:11 -0300

    Add ProductHunt badge to home screen

---
+++ src/ProductHuntBadge.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function () {
+    return (
+        <a href="https://www.producthunt.com/posts/escapista?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-escapista" target="_blank">
+            <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=278660&theme=light" alt="Escapista - Curated videos to relax and explore the world from home. | Product Hunt" style={{width: '180px', height: '40px'}} width={180} height={40} />
+        </a>
+    )
+}
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react";
 
 import { withTranslation } from 'react-i18next';
 
+import {
+    isMobile
+  } from "react-device-detect";
+
 import Art from './Art.js'
+import ProductHuntBadge from './ProductHuntBadge.js'
 
 import {
     BOTTOM_BAR_HEIGHT,
@@ -42,49 +47,55 @@ function Welcome(props) {
                 className="fixed z-10 pt-16 pl-16 font-body w-screen h-full text-teal-800 flex flex-col items-start justify-between"
                 style={{paddingBottom: BOTTOM_BAR_HEIGHT}}
             >
-                <div className={`${slideIn}`}>
+                <div className={`${slideIn} flex flex-auto flex-col`}>
                     <h1 className={logoTypography}>
                         Escapista
                     </h1>
 
-                    <div className="ml-16">
-                        <h2 className="mt-8 max-w-xl font-heading text-teal-800 whitespace-pre-line font-extrabold text-6xl leading-tight">
-                            { t('welcome-heading') }
-                        </h2>
+                    <div className="ml-16 flex justify-between flex-col flex-auto">
+                        <div>
+                            <h2 className="mt-8 max-w-xl font-heading text-teal-800 whitespace-pre-line font-extrabold text-6xl leading-tight">
+                                { t('welcome-heading') }
+                            </h2>
 
-                        <div className="mt-8 max-w-sm text-lg">
-                            <p className="mt-3">
-                                { t('welcome-body1') }
-                            </p>
+                            <div className="mt-8 max-w-sm text-lg">
+                                <p className="mt-3">
+                                    { t('welcome-body1') }
+                                </p>
 
-                            <p className="mt-3">
-                                { t('welcome-body2') }
-                            </p>
+                                <p className="mt-3">
+                                    { t('welcome-body2') }
+                                </p>
 
-                            <p className="mt-3">
-                                { t('welcome-body3') }
-                            </p>
+                                <p className="mt-3">
+                                    { t('welcome-body3') }
+                                </p>
+                            </div>
+                            
+                            <div className={`mt-8 ${slideIn} delay-200`}>
+                                <button
+                                    className="py-3 px-6 bg-teal-800 font-medium rounded text-white hover:bg-teal-800 duration-300"
+                                    style={{minWidth: 120}}
+                                    onClick={onStartClick}>
+                                    { t('welcome-cta') }
+                                </button>
+
+                                <a
+                                    href={manifestoUrl}
+                                    className="py-3 px-6 ml-2 bg-white rounded font-medium hover:bg-gray-200 duration-300"
+                                    target="_blank" rel="noopener noreferrer"
+                                >
+                                    { t('read-the-manifesto') }
+                                </a>
+                            </div>
                         </div>
                         
-                        <div className={`mt-8 ${slideIn} delay-200`}>
-                            <button
-                                className="py-3 px-6 bg-teal-800 font-medium rounded text-white hover:bg-teal-800 duration-300"
-                                style={{minWidth: 120}}
-                                onClick={onStartClick}>
-                                { t('welcome-cta') }
-                            </button>
-
-                            <a
-                                href={manifestoUrl}
-                                className="py-3 px-6 ml-2 bg-white rounded font-medium hover:bg-gray-200 duration-300"
-                                target="_blank" rel="noopener noreferrer"
-                            >
-                                { t('read-the-manifesto') }
-                            </a>
-                        </div>
+                        {
+                            !isMobile && 
+                            <ProductHuntBadge/>
+                        }
                     </div>
                 </div>
-
             </div>
 
             <div className={`overflow-hidden`}>
